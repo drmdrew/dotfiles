328de8df4331c48bd5a35a53cc53878cffeb3077
Drew MacInnis <drewmacinnis@gmail.com>
2015-08-30 17:58:35 -0400

    Detect DOTFILES directory

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 #
 # install.sh - Creates symbolic links for dotfiles in the user's 
 #              home directory.
 #
+DOTFILES=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
 
-DOTFILES=$(dirname $0)
-
+rm ~/.vimrc
 ln -v -s $DOTFILES/vimrc ~/.vimrc
 
